1240224213bec995b7710ced2754356d1003a85e
YellowFlash2012 <cyclop9@hotmail.com>
2024-06-13 08:35:07 +0100

    design and implement get_all_products view, register a serializer for product model and the rul for the view in urls.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .models import Product
+from rest_framework.response import Response
+from .serializers import ProductSerializer
 
 # Create your views here.
+@api_view(['GET'])
+def get_products(request):
+    products = Product.objects.all()
+    
+    serializer = ProductSerializer(products, many=True)
+    
+    print(len(serializer.data))
+    
+    return Response({"success":True, "message":"Here are all the products...", "count":len(serializer.data), "products":serializer.data})
+    
