083e1a5c51965801695a1e038008c7b10f5f2e1c
YellowFlash2012 <cyclop9@hotmail.com>
2024-06-14 07:46:41 +0100

    design and implement search config by updating filters.py and adding keyword, min_price, max_price params.

--- products/filters.py
+++ products/filters.py
@@ -2,7 +2,14 @@ from django_filters import rest_framework as filters
 from .models import Product
 
 class ProductsFilter(filters.FilterSet):
+    # search config
+    keyword = filters.CharFilter(field_name = "name", lookup_expr="icontains")
+    
+    min_price = filters.NumberFilter(field_name = "price" or 0,lookup_expr="gte")
+    
+    max_price = filters.NumberFilter(field_name = "price" or 1000000,lookup_expr="lte")
+    
     class Meta:
         model = Product
-        fields = ("category", "brand")
+        fields = ("keyword", "min_price", "max_price", "category", "brand")
 
