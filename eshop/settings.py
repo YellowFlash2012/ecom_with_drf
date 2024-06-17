bcb19b70151a9b112102824fe307f791097e023b
YellowFlash2012 <cyclop9@hotmail.com>
2024-06-17 07:49:51 +0100

    design and implement a custom exception handling view in utils, update settings.py to overrule the default custom_exception_handling functionality.

--- eshop/settings.py
+++ eshop/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get('DEBUG')
-DEBUG = False
+DEBUG = os.environ.get('DEBUG') == 'True'
+# DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -89,6 +89,10 @@ DATABASES = {
         'PORT': os.environ.get('DATABASE_PORT'),
         'USER': os.environ.get('DATABASE_USER'),
     }
+}
+
+REST_FRAMEWORK = {
+    "EXCEPTION_HANDLER":"eshop.utils.custom_exception_handler.custom_exception_handler"
 }
 
 
--- eshop/urls.py
+++ eshop/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path("api/v1/products", include("products.urls"))
+    path("api/v1/products/", include("products.urls"))
 ]
 
 handler404 = "eshop.utils.error_handling_views.handler404"
