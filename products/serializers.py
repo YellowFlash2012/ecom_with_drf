900dc8ab31e825fefb1f3bc4d9e7769ae47fa905
YellowFlash2012 <cyclop9@hotmail.com>
2024-06-18 07:30:06 +0100

    update ProductSerialzer to display the list of images relevant with each product

--- products/serializers.py
+++ products/serializers.py
@@ -2,13 +2,13 @@
 from rest_framework import serializers
 
 from .models import Product, ProductImages
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = "__all__"
-
 class ProductImagesSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductImages
         fields = "__all__"
+
+class ProductSerializer(serializers.ModelSerializer):
+    images = ProductImagesSerializer(many=True, read_only=True)
+    class Meta:
+        model = Product
+        fields = ("id", "name", "description", "price", "brand", "category", "stock", "user", "images")
