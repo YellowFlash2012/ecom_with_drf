90cf669e98506fb29dcd9ff9a268ad356b495120
YellowFlash2012 <cyclop9@hotmail.com>
2024-06-19 17:10:37 +0100

    design and implement register_user view, create a urls.py file in users account and update it with the register_user endpoint, then update urls.py in eshop project. Test the register user view in postman.

--- eshop/urls.py
+++ eshop/urls.py
@@ -6,7 +6,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path("api/v1/products/", include("products.urls"))
+    path("api/v1/products/", include("products.urls")),
+    
+    path("api/v1/users/", include("users.urls")),
 ]
 
 handler404 = "eshop.utils.error_handling_views.handler404"
--- users/views.py
+++ users/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+from .serializers import RegisterSerializer
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
+@api_view(["POST"])
+def register_user(request):
+    data = request.data
+    
+    user = RegisterSerializer(data=data)
+    
+    if user.is_valid():
+        if not User.objects.filter(username=data["email"]).exists():
+            user = User.objects.create(
+                first_name = data["first_name"],
+                last_name = data["last_name"],
+                email = data["email"],
+                username = data["email"],
+                password = make_password(data["password"]),
+            )
+            
+            return Response({"success":True, "message": f"Welcome aboard, {user.first_name}!"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"success":False, "message":"A user with this email already exists!"}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response(user.error, status=status.HTTP_400_BAD_REQUEST)
