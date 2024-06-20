f00001eb49df52dac5a41b356f20d1b196855019
YellowFlash2012 <cyclop9@hotmail.com>
2024-06-20 07:15:19 +0100

    design and implement login-user functionality by adding simpleJWT config to eshop/urls.py

--- eshop/urls.py
+++ eshop/urls.py
@@ -2,6 +2,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,6 +14,10 @@ urlpatterns = [
     path("api/v1/products/", include("products.urls")),
     
     path("api/v1/users/", include("users.urls")),
+    
+    # simpleJWT config
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 handler404 = "eshop.utils.error_handling_views.handler404"
