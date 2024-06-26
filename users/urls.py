d72019c5a7466750ea9fb2a2da0385e173f6d12a
YellowFlash2012 <cyclop9@hotmail.com>
2024-06-26 07:56:58 +0100

    design and implement forget_password view, update urls.py with the new endpoint. Update models.py with save_profile signal. Create a new user and test the forget_password functionality with that new user in postman. Testing that functionality with an old user will lead to error: profile NOT found. Then design and implement reset_password view, update urls.py and test it in postman.

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,14 @@ from . import views
 
 urlpatterns = [
     path("register", views.register_user, name="register_user"),
+    
     path("profile", views.current_user, name="current_user"),
+    
     path("profile/update", views.update_user_details, name="update_user_details"),
+    
+    path("forgot_password", views.forgot_password, name="forgot_password"),
+    
+    path("reset_password/<str:token>", views.reset_password, name="reset_password"),
     
     
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
@@ -8,6 +8,10 @@ from rest_framework.permissions import IsAuthenticated
 from .serializers import RegisterSerializer, UserSerializer
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
+from django.utils.crypto import get_random_string
+from django.shortcuts import get_object_or_404
+from django.core.mail import send_mail
+from datetime import timedelta, datetime
 
 # Create your views here.
 @api_view(["POST"])
@@ -60,3 +64,61 @@ def update_user_details(request):
     serializer = UserSerializer(user, many=False)
     
     return Response({"success":True, "message": "Your details were successfully updated!", "data":serializer.data}, status=status.HTTP_201_CREATED)
+
+# password reset views
+def get_current_host(request):
+    protocol = request.is_secure() and "https" or "http"
+    host = request.get_host()
+    
+    return "{protocol}://{host}/".format(protocol=protocol, host=host)
+
+@api_view(["POST"])
+def forgot_password(request):
+    data = request.data
+    
+    user = get_object_or_404(User, email=data["email"])
+    
+    token = get_random_string(39)
+    expire_data = datetime.now() + timedelta(minutes=3)
+    
+    # from Profile model
+    user.profile.reset_password_token = token
+    user.profile.reset_password_expire = expire_data
+    
+    user.profile.save()
+    
+    host = get_current_host(request)
+    
+    link = "{host}api/v1/users/reset_pw/{token}".format(host=host, token=token)
+    
+    body = "Your password reset link is: {link}".format(link=link)
+    
+    send_mail(
+        "Password reset for eShop",
+        body,
+        "noreply@eshop.com",
+        [data["email"]]
+    )
+    
+    return Response({"success":True, "message":"Password reset email sent to: {email}".format(email=data["email"])})
+
+@api_view(["POST"])
+def reset_password(request, token):
+    data = request.data
+    
+    user = get_object_or_404(User, profile__reset_password_token=token)
+    
+    if user.profile.reset_password_expire.replace(tzinfo=None) < datetime.now():
+        return Response({"success":False, "error":"token has expired"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    if data["password"] != data["confirmPassword"]:
+        return Response({"success":False, "error":"The passwords do NOT match!"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    user.password = make_password(data["password"])
+    user.profile.reset_password_token = ""
+    user.profile.reset_password_expire = None
+    
+    user.profile.save()
+    user.save()
+    
+    return Response({"success":True, "message":"Your password was successfully changed!"}, status=status.HTTP_201_CREATED)
